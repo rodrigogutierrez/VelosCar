185615b8aa9882947940a1b1af0f6c45742f1ba6
rodrigogutierrez <rodrigo.gutierrez1@virginiogomez.cl>
2014-05-31 19:02:09 -0400

    Se agregó layout de administracion, y se fragmentó. Ademas se agregaron las hojas de estilo.

--- VelosCar/VelosCar/App_Start/RouteConfig.cs
+++ VelosCar/VelosCar/App_Start/RouteConfig.cs
@@ -14,9 +14,10 @@ namespace VelosCar
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "raiz",
+                url: "",
+                defaults: new { controller = "Home", action = "Index"},
+                constraints: new { httpMethod = new HttpMethodConstraint(new string[] { "GET" }) }
             );
         }
     }
