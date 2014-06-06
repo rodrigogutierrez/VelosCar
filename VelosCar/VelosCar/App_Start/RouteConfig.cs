b05085fd6aad99a15c55a69c4004f9ffc30e2986
rodrigogutierrez <rodrigo.gutierrez1@virginiogomez.cl>
2014-06-05 22:47:44 -0400

    Comence a escribir las rutas de la aplicacion

--- VelosCar/VelosCar/App_Start/RouteConfig.cs
+++ VelosCar/VelosCar/App_Start/RouteConfig.cs
@@ -15,9 +15,54 @@ namespace VelosCar
 
             routes.MapRoute(
                 name: "raiz",
-                url: "algo",
+                url: "",
                 defaults: new { controller = "Home", action = "Index"},
                 constraints: new { httpMethod = new HttpMethodConstraint(new string[] { "GET" }) }
             );
+
+            //rutas del vehiculo
+
+            //listar vehiculos
+            routes.MapRoute(
+                name: "vehiculos",
+                url: "vehiculos",
+                defaults: new { controller = "Vehiculo", action = "Index"},
+                constraints: new { httpMethod = new HttpMethodConstraint(new string[] { "GET" }) }
+            );
+
+            //registrar vehiculos
+            routes.MapRoute(
+                name: "registrar_vehiculos",
+                url: "vehiculos/registrar",
+                defaults: new { controller = "Vehiculo", action = "Registrar"},
+                constraints: new { httpMethod = new HttpMethodConstraint(new string[] { "GET" }) }
+            );
+
+            //editar vehiculos
+            routes.MapRoute(
+                name: "editar_vehiculo",
+                url: "vehiculos/{id}/registrar",
+                defaults: new { controller = "Vehiculo", action = "Editar"},
+                constraints: new { httpMethod = new HttpMethodConstraint(new string[] { "GET" }) }
+            );
+
+            //editar vehiculos
+            routes.MapRoute(
+                name: "ver_vehiculo",
+                url: "vehiculos/{id}",
+                defaults: new { controller = "Vehiculo", action = "Ver"},
+                constraints: new { httpMethod = new HttpMethodConstraint(new string[] { "GET" }) }
+            );
+                //rutas del tipo de vehiculo
+                //rutas del combustible
+                //rutas de los accesorios
+                //rutas de la marca de vehiculo
+                //rutas del equipamiento
+            //rutas del usuarios
+            //rutas de la reserva
+            //rutas de las tarifas
+            //rutas de las sucursales
+
+            
         }
     }
