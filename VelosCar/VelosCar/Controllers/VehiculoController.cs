f7d36cb5da10d7a8d54d7aaaa99243993e4d4f88
alecanios <jose.canio@virginiogomez.cl>
2014-06-05 22:58:23 -0400

    Se crea Controlador Vehiculos.

---
+++ VelosCar/VelosCar/Controllers/VehiculoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VelosCar.Controllers
+{
+    public class VehiculoController : Controller
+    {
+        //
+        // GET: /Vehiculo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        public ActionResult Crear()
+        {
+            return View();
+        }
+
+        public ActionResult Actualizar()
+        {
+            return View();
+        }
+    }
+}
