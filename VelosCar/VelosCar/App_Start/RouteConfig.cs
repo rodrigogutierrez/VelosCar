a45fa99feed745e23f268623213b723ec0acf117
rodrigogutierrez <rodrigo.gutierrez1@virginiogomez.cl>
2014-06-07 11:43:12 -0400

    mas rutas

--- VelosCar/VelosCar/App_Start/RouteConfig.cs
+++ VelosCar/VelosCar/App_Start/RouteConfig.cs
@@ -370,11 +370,6 @@ namespace VelosCar
 
             //rutas de las tarifas
             //rutas de las sucursales
-<<<<<<< HEAD
-            //rutas de los informes estadisticos
-=======
-            //rutas de los informes
->>>>>>> 7570b149b19122c2b7be23d5004afd768dd30f69
 
         }
     }
