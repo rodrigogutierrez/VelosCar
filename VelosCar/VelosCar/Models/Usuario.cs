404350f731df5139b31d660b181fb788cc0fa3c5
roguben <roguben@outlook.com>
2014-06-08 21:27:08 -0400

    completando mas clases

--- VelosCar/VelosCar/Models/Usuario.cs
+++ VelosCar/VelosCar/Models/Usuario.cs
@@ -11,14 +11,32 @@ namespace VelosCar.Models
     public class Usuario
     {
         public int Id { get; set; }
+
+        [Required]
         public string RutPasaporte { get; set; }
+
+        [Required]
         public string Nombres { get; set; }
+
+        [Required]
         public string ApellidoPaterno { get; set; }
+
+        [Required]
         public string ApellidoMaterno { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string Pais { get; set; }
+
+        [Required]
         public string Telefono { get; set; }
+
+        [Required]
         public string Contrasena { get; set; }
+
+        [Required]
         public string Estado { get; set; }
 
         //declaracion de la relacion entre usuario y reserva
