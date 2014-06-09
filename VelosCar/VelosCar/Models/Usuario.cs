01b6da20d42bb0fb08ad7f2970df2baa28555d7a
roguben <roguben@outlook.com>
2014-06-08 21:12:26 -0400

    Se agregan datos a las clases usuario, vehiculo, veloscarcontext

--- VelosCar/VelosCar/Models/Usuario.cs
+++ VelosCar/VelosCar/Models/Usuario.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace VelosCar.Models
 {
+    [Table("Usuarios")]
     public class Usuario
     {
+        public int Id { get; set; }
+        public string RutPasaporte { get; set; }
+        public string Nombres { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Email { get; set; }
+        public string Pais { get; set; }
+        public string Telefono { get; set; }
+        public string Contrasena { get; set; }
+        public string Estado { get; set; }
+
+        //declaracion de la relacion entre usuario y reserva
+        public virtual ICollection<Reserva> Reserva { get; set; }
+
     }
 }
--- VelosCar/VelosCar/Models/VelosCarContext.cs
+++ VelosCar/VelosCar/Models/VelosCarContext.cs
@@ -9,6 +9,14 @@ namespace VelosCar.Models
     public class VelosCarContext : DbContext
     {
         public DbSet<Accesorio> Accesorios { get; set; }
-        public int MyProperty { get; set; }
+        public DbSet<Combustible> Combustibles { get; set; }
+        public DbSet<Equipamiento> Equipamientos { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Reserva> Reservas { get; set; }
+        public DbSet<Sucursal> Sucursales { get; set; }
+        public DbSet<Tarifa> Tarifas { get; set; }
+        public DbSet<TipoVehiculo> TiposVehiculo { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Vehiculo> Vehiculos { get; set; }
     }
 }
