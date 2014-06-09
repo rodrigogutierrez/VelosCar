67a6934b5487cab08cb3cc8a7797e0381cd514a2
roguben <roguben@outlook.com>
2014-06-08 20:15:36 -0400

    Se agregaron las clases

---
+++ VelosCar/VelosCar/Models/VelosCarContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VelosCar.Models
+{
+    public class VelosCarContext : DbContext
+    {
+        public DbSet<Accesorio> Accesorios { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
