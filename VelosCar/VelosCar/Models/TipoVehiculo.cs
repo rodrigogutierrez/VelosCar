1a967466e4d6a0f5704901d07f41e37e68fd8545
rodrigogutierrez <rodrigo.gutierrez1@virginiogomez.cl>
2014-06-10 19:39:31 -0400

    Agregados atributos de la tarifa

--- VelosCar/VelosCar/Models/TipoVehiculo.cs
+++ VelosCar/VelosCar/Models/TipoVehiculo.cs
@@ -7,5 +7,11 @@ namespace VelosCar.Models
 {
     public class TipoVehiculo
     {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public int Valor { get; set; }
+
+        //declaracion de la relacion entre la tarifa y el vehiculo
+        public virtual ICollection<Vehiculo> Vehiculos { get; set; }
     }
 }
