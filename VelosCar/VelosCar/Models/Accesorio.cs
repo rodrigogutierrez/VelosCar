5fee5f4beed800cf4af76d3457825ba2a591c844
alecanios <jose.canio@virginiogomez.cl>
2014-06-10 20:04:24 -0400

    Se Modifica el model tablas accesorio, combustible,equipamiento

--- VelosCar/VelosCar/Models/Accesorio.cs
+++ VelosCar/VelosCar/Models/Accesorio.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace VelosCar.Models
 {
+    [Table ("Accesorios")]
     public class Accesorio
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public int Valor { get; set; }
     }
 }
